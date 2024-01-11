30536bb9f24a8523a04673574998eb35e47891bb
Prajyot Karnik <prajyotkcsu@gmail.com>
2024-01-10 22:19:51 -0600

    first commit

--- src/main/java/findnearme/findnearme/Config.java
+++ src/main/java/findnearme/findnearme/Config.java
@@ -1,5 +1,6 @@
 package findnearme.findnearme;
 
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
