b2a53a19a59ec1bc60e0b07294131ea92427646e
Prajyot Karnik <prajyotkcsu@gmail.com>
2024-02-29 23:58:28 -0600

    multi-node cluster

--- src/main/java/findnearme/findnearme/Controller.java
+++ src/main/java/findnearme/findnearme/Controller.java
@@ -11,6 +11,8 @@ public class Controller {
 
     @PostMapping("/create")
     public void createRestaurant(@RequestBody Restaurant restaurant){
+        int zip=94542;
+        System.out.println(zip%3);
         restaurantRepository.save(restaurant);
     }
     /**
@@ -19,20 +21,20 @@ public class Controller {
      * same input-> 12.32, 53.56, [noodles, asian, thai]
      * sample output-> restaurant name: full address
      */
-    @GetMapping("/search")
-    public void searchRestaurants(
-            @RequestParam String category,
-            @RequestParam Double lat,
-            @RequestParam Double lon) {
-        GeoPoint location = new GeoPoint(33.00167393830364, -96.70512880312421);
-
-        // Define the search query
-        SearchQuery query = new NativeSearchQueryBuilder()
-                .withFilter(QueryBuilders.geoDistanceQuery("pin.location").point(location).distance("4km"))
-                .build();
-
-        // Execute the search query using the repository
-        return restaurantRepository.search(query);
-        //return restaurantRepository.findByCategoryAndLocation(category, lat, lon);
-    }
+//    @GetMapping("/search")
+//    public void searchRestaurants(
+//            @RequestParam String category,
+//            @RequestParam Double lat,
+//            @RequestParam Double lon) {
+//        GeoPoint location = new GeoPoint(33.00167393830364, -96.70512880312421);
+//
+//        // Define the search query
+//        SearchQuery query = new NativeSearchQueryBuilder()
+//                .withFilter(QueryBuilders.geoDistanceQuery("pin.location").point(location).distance("4km"))
+//                .build();
+//
+//        // Execute the search query using the repository
+//        return restaurantRepository.search(query);
+//        //return restaurantRepository.findByCategoryAndLocation(category, lat, lon);
+//    }
 }
